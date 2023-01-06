7cf0d6d5a9bcd898c5dd79e3cf2c3b8a3062605a
GitHub Terraform <zscaler-github-P@zscaler.com>
2023-01-06 15:51:11 -0800

    update activation.sh path

--- main.tf
+++ main.tf
@@ -16,7 +16,7 @@ resource "null_resource" "activation" {
     version = local.timestamp
   }
   provisioner "local-exec" {
-    command = "./activation.sh"
+    command = "/bin/bash activation.sh"
   }
   depends_on = [zia_firewall_filtering_ip_source_groups.this]
 }
